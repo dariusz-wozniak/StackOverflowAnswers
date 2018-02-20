5805b4756ddb9f24655a61d501949b354e96cbf7
Dariusz Woźniak <dariusz.wozniak@gmail.com>
2018-02-20 22:56:11 +0100

    Answer for — Using NUnit TestRunner with xml filter
    https://stackoverflow.com/questions/48893789/using-nunit-testrunner-with-xml-filter/48894749#48894749

---
+++ NUnitTestRunner/using-nunit-testrunner-with-xml-filter/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Reflection;
+using NUnit.Engine;
+using NUnit.Framework;
+
+namespace NUnitTestRunner
+{
+    public class Class1
+    {
+        [Test]
+        public void Test123() => Assert.Pass();
+    }
+
+    /// <summary>
+    /// https://stackoverflow.com/questions/48893789/using-nunit-testrunner-with-xml-filter
+    /// </summary>
+    public class Program
+    {
+        [Test]
+        [Explicit]
+        public static void Main(string[] args)
+        {
+            var path = Assembly.GetExecutingAssembly().Location;
+
+            var package = new TestPackage(path);
+            package.AddSetting("WorkDirectory", Environment.CurrentDirectory);
+
+            using (ITestEngine engine = TestEngineActivator.CreateInstance())
+            {
+                var filterService = engine.Services.GetService<ITestFilterService>();
+
+                var builder = filterService.GetTestFilterBuilder();
+                builder.AddTest("NUnitTestRunner.Class1.Test123");
+                var filter = builder.GetFilter();
+
+                using (ITestRunner runner = engine.GetRunner(package))
+                {
+                    var result = runner.Run(listener: null, filter: filter);
+                }
+            }
+        }
+    }
+}
