3b63ca045f7e2ac595a59689756d059bddd97054
Dariusz Woźniak <dariusz.wozniak@gmail.com>
2018-02-21 20:03:17 +0100

    Answer for — C# Converting a string containing a floating point to an integer
    https://stackoverflow.com/questions/3395381/c-sharp-converting-a-string-containing-a-floating-point-to-an-integer/3395473

---
+++ StackOverflowAnswers/csharp/converting-a-string-containing-a-floating-point-to-an-integer.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace StackOverflowAnswers.csharp
+{
+    /// <summary>
+    /// https://stackoverflow.com/questions/3395381/c-sharp-converting-a-string-containing-a-floating-point-to-an-integer/3395473#3395473
+    /// </summary>
+    public class ConvertingAStringContainingAFloatingPointToAnInteger
+    {
+        [Test]
+        [SetCulture("en-US")]
+        public void Repro1()
+        {
+            var a = (int)Convert.ToDouble("1.2");
+            a.Should().Be(1);
+        }
+
+        [Test]
+        public void Repro2()
+        {
+            var a = (int)Convert.ToDouble("1.2", CultureInfo.InvariantCulture.NumberFormat);
+            a.Should().Be(1);
+        }
+
+        [Test]
+        public void Repro3()
+        {
+            var a = int.Parse("1.2".Split('.')[0]);
+            a.Should().Be(1);
+        }
+
+        [Test]
+        public void Repro4()
+        {
+            var a = int.Parse("1.2".Split('.').First());
+            a.Should().Be(1);
+        }
+    }
+}
